a4170de5cc359b2f59759d85e09293c381236456
Socorro Dominguez Vidana <42593920+sedv8808@users.noreply.github.com>
2022-01-31 20:16:09 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,3 @@
 install.packages('leaflet')
-install.packages('devtools')
-library(devtools)
-devtools::install_github('NeotomaDB/neotoma2')
+install.packages("githubinstall")
+githubinstall::githubinstall('NeotomaDB/neotoma2')
